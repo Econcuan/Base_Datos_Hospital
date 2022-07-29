7122de79edf023cf51371bcb06526f6144705045
Econcuan <Estuardogomez_1997@hotmail.com>
2022-07-28 23:17:47 -0600

    Se crea insert en tabla turno

--- ALTA_Turnos.sql
+++ ALTA_Turnos.sql
@@ -1,9 +1,16 @@
 
-
+/*
 select * from Turno
+select * from paciente
 select * from TurnoPaciente
+select * from medico
+insert into medico values('Pablo','Ramirez')
+*/
 
-CREATE PROC ALTA_Turno(
+EXEC ALTA_Turno '20190215 08:15',2,7,'El paciente tiene que estar en Ayunas'
+
+
+ALTER PROC ALTA_Turno(
 		@fecha char(14), --20190215 12:00
 		@idPaciente paciente,
 		@idmedico medico,
@@ -23,8 +30,11 @@ BEGIN
 	INSERT INTO Turno(fechaTurno, estado, observacion)
 	VALUES (@fecha,0,@observacion)
 
+	declare @auxIdturno turno
+	set @auxIdturno = @@IDENTITY
+
 	INSERT INTO TurnoPaciente (idTurno, idPaciente, idMedico)
-	VALUES (@idturno,@idPaciente,@idmedico)
+	VALUES (@auxIdturno,@idPaciente,@idmedico)
 
 	PRINT 'El turno se agrego correctamente'
 	Return
