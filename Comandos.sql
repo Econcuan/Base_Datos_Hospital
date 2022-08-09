12e052fc1d0437235f3d1db2923b1dff392049ce
Econcuan <Estuardogomez_1997@hotmail.com>
2022-08-09 00:21:09 -0600

    se agregan cambios y nuevos comandos

--- Comandos.sql
+++ Comandos.sql
@@ -1,8 +1,35 @@
 	 --COMANDOS 
 
+
+--FUncion LEFT 
+Declare @var1 varchar(20)
+	set @var1 = 'Ramiro'
+Print LEFT(@Var1,2)
+
+--FUncion RIGHT
+Declare @var2 varchar(20)
+	set @var2 = 'Ramiro'
+Print RIGHT(@Var2,2)
+
 EXEC S_paciente 5
 
+/*Uso sentencia ALTER, DROP Y TRUNCATE 
+ALTER TABLE Paciente ADD estado smallint 
+ALTER TABLE Paciente ALTER COLUMN estado bit 
+ALTER TABLE Paciente DROP COLUMN estado
+*/
 
+--DROP Sirve para eliminar tablas 
+CREATE table ejemplo (Campo1 int, campo2 int)
+DROP TABLE ejemplo
+
+--TRUNCATE Sirve para eliminar registors de una tabla
+CREATE table ejemplo (Campo0 int IDENTITY (1,1), campo1 int, campo2 int)
+select * from ejemplo
+insert into ejemplo values (2,3)
+TRUNCATE TABLE ejemplo 
+
+CREATE TYPE Pais FROM CHAR(3)	
 
  --Select
 SELECT * FROM Paciente
